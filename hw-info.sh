52bbe7c0b25bd47e44eb8f9ec7a43df0b4f87ff0
Kordian Witek <code@kordy.com>
2020-04-16 20:30:57 -0400

    documentation updates

--- hw-info.sh
+++ hw-info.sh
@@ -3,11 +3,13 @@
 # - works on any OS that can run bash
 # - tested on Linux, MacOS & Cygwin
 #
-# * By Kordian Witek <code@kordian.com>
+# * By Kordian Witek <code [at] kordy.com>
 #
 
 # DOCUMENTATION:
-# Provides the following information as a 1-line:
+#
+# Provides the following information as a 1-line in CSV format:
+#
 # - hostname (and domain)
 # - OS type
 # - OS name (eg: Ubuntu) and version
@@ -16,13 +18,31 @@
 # - Bare Metal or VM
 # - HW type & model
 # - How much RAM (in GB)
-# - How many CPUs
-# - Real or virtual CPUs?
+# - How many CPUs (or cores/threads)
+# - Real or virtual CPUs? (CPUs vs vCPUs)
 # - CPU model/type and CPU speed (in GHz)
 # - 32bit or 64bit system?
 # - Local Disk sizes (in human friendly format)
-# - FS type
-# - When was the OS built?
+# - FS type (eg: ext4, ntfs)
+# - When was the OS built? (based on dates of some key root files)
+#
+# Examples on running on various Operating Systems:
+#
+# $ ./hw-info.sh
+#
+# Linux (Ubuntu & Mint):
+# speedy: Linux Ubuntu 18.04.4 LTS Bionic Beaver/'20, KVM: pc-q35-3.1 QEMU Standard PC (Q35+ICH9, 2009), 4GB RAM, 2 x vCPU E5-2680 v3 @ 2.50GHz, 64bit, 77.2G+2.5G Disk/ext4, Built Apr'14
+#
+# laptop-pc (t480s): Linux Mint 19.3 Tricia/'19, BareMetal: Lenovo ThinkPad T480s, 15GB RAM, 8 x CPU i7-8650U @ 1.90GHz, 64bit, 119.5G Disk/overlay, Built Aug'07
+#
+# Cygwin:
+# Speedy-PC: Cygwin 3.0.7/'19, BareMetal: Dell XPS 8900, 32GB RAM, 8 x CPU i7-6700 @ 3.40GHz, 32bit, 477G+932G+7.3T Disk/ntfs, Built Sep'17
+#
+# Raspbian:
+# pi-hole: Linux Raspbian 9 Stretch/'19, BareMetal: RaspberryPi 3 B+ Rev 1.3, 1GB RAM, 4 x CPU ARMv7 Rev4 (v7l) 1.4GHz, 59.6G Disk/ext4, Built Nov'18
+#
+
+##################################################
 
 # store LSCPU info, as we will use it quite often
 LSCPU=/tmp/lscpu-$$
